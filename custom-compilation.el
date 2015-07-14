5ed36c1b553367c6d47595c38aacf124de8b4182
rocky <rb@dustyfeet.com>
2015-07-14 05:49:23 -0400

    Customized compilation modes that we have got so far

---
+++ custom-compilation.el
@@ -0,0 +1,5 @@
+(ignore-errors
+  (require 'ansi-color)
+  (defun my-colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max)))))
